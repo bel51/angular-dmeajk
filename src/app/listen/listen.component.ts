ee19a2b5e6f0c2b1129ec7643e21c6563344b457
bel51 <bel57622@gmail.com>
2019-08-28 11:22:19 +0200

    2 files modified

--- src/app/listen/listen.component.ts
+++ src/app/listen/listen.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listen.component.css']
 })
 export class ListenComponent implements OnInit {
+ende:number = 10;
+liste: Array<number> = [1,2];
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
+
   }
+  zaehle(){
+    this.liste = [];
+
+    for(let i = 0; i <= this.ende; i=i+1){
+      this.liste.push(i);
+    }
+  }
+
+
 
 }
